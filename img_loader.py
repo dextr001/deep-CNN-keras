3893b7a6240814ab62f7dc9d32781393dc994b5b
Richard Teammco <teammco@cs.utexas.edu>
2016-03-01 16:31:19 -0600

    Image loader commented (for reading data paths).

--- img_loader.py
+++ img_loader.py
@@ -2,33 +2,82 @@
 
 
 class ImageData(object):
-  """Defines an image data object."""
+  """Loads image file paths from input files."""
 
-  def __init__(self):
-    """?"""
+  self._DEFAULT_IMG_DIMENSIONS = (256, 256, 1)
+
+  def __init__(self)
+    """Sets up the initial data variables."""
+    # Initialize the data lists.
+    self.img_dimensions = self._DEFAULT_IMG_DIMENSIONS
     self.classnames = []
     self.train_img_files = []
     self.test_img_files = []
-    self.img_dimensions = (256, 256, 3)
 
   def set_image_dimensions(self, width, height, num_channels):
-    """?"""
+    """Set the training and testing image dimensions.
+    
+    All images fed into the neural network, for training and testing, will be
+    formatted to match these dimensions.
+
+    Args:
+      width: a positive integer indicating the width of the images.
+      height: a positive integer indicating the height of the images.
+      num_channels: the number of channels to use. This number should be 1
+          for grayscale training images or 3 to train on full RGB data.
+    """
+    # Make sure all the data is valid.
+    if width <= 0:
+      width = self._DEFAULT_IMG_DIMENSIONS[0]
+    if height <= 0:
+      height = self._DEFAULT_IMG_DIMENSIONS[1]
+    if num_channels not in [1, 3]:
+      num_channels = self._DEFAULT_IMG_DIMENSIONS[2]
+    # Set the dimensions.
     self.img_dimensions = (width, height, num_channels)
 
   def load_image_classnames(self, fname):
-    """?"""
+    """Reads the classnames for the image data from the given file.
+    
+    Each class name should be on a separate line.
+
+    Args:
+      fname: the name of the file containing the list of class names.
+    """
     self._read_file_data(fname, self.classnames)
 
   def load_train_image_paths(self, fname):
-    """?"""
+    """Reads the image paths for the training data from the given file.
+    
+    Each file name (full directory path) should be on a separate line. The file
+    paths must be ordered by their class label.
+
+    Args:
+      fname: the name of the file containing the training image paths.
+    """
     self._read_file_data(fname, self.train_img_files)
 
   def load_test_image_paths(self, fname):
-    """?"""
+    """Reads the image paths for the test data from the given file.
+    
+    Each file name (full directory path) should be on a separate line. The file
+    paths must be ordered by their class label.
+
+    Args:
+      fname: the name of the file containing the test image paths.
+    """
     self._read_file_data(fname, self.test_img_files)
 
   def _read_file_data(self, fname, destination):
-    """?"""
+    """Reads the data of the given file into the destination list.
+    
+    The file will be read line by line, and each line that doesn't start with a
+    "#" or is not empty will be stored in the given list.
+
+    Args:
+      fname: the name of the file to read.
+      destination: a list into which the file's data will be put line by line.
+    """
     paths_file = open(fname, 'r')
     for line in paths_file:
       impath = line.strip()
@@ -41,6 +90,21 @@ class ImageData(object):
 #class ImageLoader(object):
 #  """Reads images"""
 #
+#    
+#  def __init__(self, num_classes, num_train_imgs_per_class,
+#               num_test_imgs_per_class):
+#    """
+#    Args:
+#      num_classes: the number of data classes in the data set.
+#      num_train_imgs_per_class: the number of training images provided for each
+#          class. The number of images must be the same for each class. For
+#          example, if this number is 100, then for each of the 'num_classes'
+#          classes, exactly 100 training images must be provided.
+#      num_test_imgs_per_class: similarly, the number of test images per class.
+#    """
+#    self._num_classes = num_classes
+#    self._num_train_imgs_per_class = num_train_img_per_class
+#    self._num_test_imgs_per_class = num_test_img_per_class
 #  def read_images(self, im_names_file, img_dimensions, data, labels, classes,
 #                  disp='all'):
 #    """Reads all images."""
