eafd51a34dd6deb89691a7dcc738bf8b8e5b7973
Richard Teammco <teammco@cs.utexas.edu>
2016-03-02 09:25:36 -0600

    Added the model to the run script.
    Everything is set up.

--- model.py
+++ model.py
@@ -1,15 +1,62 @@
-# Trains a deep CNN on a subset of ImageNet data.
-#
-# Run with GPU:
-#   THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python model.py
-#
-# This example is from here:
-#   https://github.com/fchollet/keras/blob/master/examples/cifar10_cnn.py
-#
-# CNN layer classes documentation:
-#   http://keras.io/layers/convolutional/
-#
-#
+# The build_model() function returns the model as defined by the code in this
+# file. Modify the architecture as needed.
+
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.core import Activation, Dense, Dropout, Flatten
+from keras.models import Sequential
+
+
+def build_model(img_channels, img_w, img_h):
+  """Builds and returns a learning model.
+
+  Update the code as needed. TODO: possibly parse this from a text file.
+
+  Args:
+    img_channels: the number of channels in the input images (1 for grayscale,
+        or 3 for RGB).
+    img_w: the width (in pixels) of the input images.
+    img_h: the height of the input images.
+
+  Returns:
+    A deep neural network model.
+  """
+  # Build the CNN model.
+  model = Sequential()
+  
+  # Add a convolution layer:
+  model.add(Convolution2D(32, 3, 3, border_mode='same',
+                          input_shape=(img_channels, img_w, img_h)))
+  model.add(Activation('relu'))
+  
+  # And another one:
+  model.add(Convolution2D(32, 3, 3))
+  model.add(Activation('relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Dropout(0.25))
+  
+  # Add another convolution layer:
+  model.add(Convolution2D(64, 3, 3, border_mode='same'))
+  model.add(Activation('relu'))
+  
+  # And yet another:
+  model.add(Convolution2D(64, 3, 3))
+  model.add(Activation('relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Dropout(0.25))
+  
+  # Add a fully-connected layer:
+  model.add(Flatten())
+  model.add(Dense(512))
+  model.add(Activation('relu'))
+  model.add(Dropout(0.5))
+  
+  # Add a final softmax output layer:
+  model.add(Dense(num_classes))
+  model.add(Activation('softmax'))
+
+  return model
+
+
 #################################### NOTES #####################################
 # Convolution2D:
 #     nb_filter,    # number of filters
@@ -34,92 +81,3 @@
 # Flatten:
 #     Flattens the input
 ################################################################################
-
-
-from __future__ import print_function
-from img_loader import ImageInfo, ImageLoader
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.core import Activation, Dense, Dropout, Flatten
-from keras.models import Sequential
-from keras.optimizers import SGD
-from keras.preprocessing.image import ImageDataGenerator
-import time
-
-
-# Hyperparameters:
-batch_size = 32
-num_epochs = 50 #200
-data_augmentation = False
-learning_rate = 0.01
-decay = 1e-6
-momentum = 0.9
-
-# Set the data parameters and load the images (preprocessed appropriately):
-img_info = ImageInfo(25, 100, 20)
-img_info.set_image_dimensions(128, 128, 1)
-img_info.load_image_classnames('classnames.txt')
-img_info.load_image_classnames('classnames.txt')
-img_info.load_image_classnames('classnames.txt')
-
-start_time = time.time()
-(train_data, train_labels), (test_data, test_labels) = load_data((img_w, img_h))
-elapsed = round(time.time() - start_time, 2)
-print ('Data successfully loaded in {} seconds.'.format(elapsed))
-
-#
-## Build the CNN model.
-#model = Sequential()
-#
-## Add a convolution layer:
-#model.add(Convolution2D(32, 3, 3, border_mode='same',
-#                        input_shape=(img_channels, img_w, img_h)))
-#model.add(Activation('relu'))
-#
-## And another one:
-#model.add(Convolution2D(32, 3, 3))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
-#
-## Add another convolution layer:
-#model.add(Convolution2D(64, 3, 3, border_mode='same'))
-#model.add(Activation('relu'))
-#
-## And yet another:
-#model.add(Convolution2D(64, 3, 3))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
-#
-## Add a fully-connected layer:
-#model.add(Flatten())
-#model.add(Dense(512))
-#model.add(Activation('relu'))
-#model.add(Dropout(0.5))
-#
-## Add a final softmax output layer:
-#model.add(Dense(num_classes))
-#model.add(Activation('softmax'))
-#
-#
-## Compile the model with SGD + momentum.
-#print ('Compiling module...')
-#start_time = time.time()
-#sgd = SGD(lr=learning_rate, decay=decay, momentum=momentum, nesterov=True)
-#model.compile(loss='categorical_crossentropy', optimizer=sgd)
-#elapsed = round(time.time() - start_time, 2)
-#print ('Done in {} seconds.'.format(elapsed))
-#
-## Train the model.
-#start_time = time.time()
-#if not data_augmentation:
-#  print ('Training without data augmentation.')
-#  model.fit(train_data, train_labels,
-#            validation_data=(test_data, test_labels), batch_size=batch_size,
-#            nb_epoch=num_epochs, shuffle=True,
-#            show_accuracy=True, verbose=1)
-#else:
-#  print ('Training with additional data augmentation.')
-#  # TODO: implement this!
-#elapsed = round(time.time() - start_time, 2)
-#print ('Finished training in {} seconds.'.format(elapsed))
--- run.py
+++ run.py
@@ -1,23 +1,65 @@
+# Trains a deep CNN on a subset of ImageNet data.
+#
+# Run with GPU:
+#   THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python model.py
+#
+# This example is from here:
+#   https://github.com/fchollet/keras/blob/master/examples/cifar10_cnn.py
+#
+# CNN layer classes documentation:
+#   http://keras.io/layers/convolutional/
+#
+#
 from img_loader import ImageInfo, ImageLoader
+from keras.optimizers import SGD
+from model import build_model
 import time
+
 
 # Hyperparameters:
 batch_size = 32
-num_epochs = 50 #200
+num_epochs = 10 #50 #200
 data_augmentation = False
 learning_rate = 0.01
 decay = 1e-6
 momentum = 0.9
 
-# Set the data parameters and load the images (preprocessed appropriately):
+# Set the data parameters and image source paths.
 img_info = ImageInfo(25, 100, 20)
-img_info.set_image_dimensions(128, 128, 1)
+img_info.set_image_dimensions(128, 128, 1)  # img width, height, channels
 img_info.load_image_classnames('test/classnames.txt')
 img_info.load_train_image_paths('test/trainImNames.txt')
 img_info.load_test_image_paths('test/test1ImNames.txt')
 
+# Load the images into memory and preprocess appropriately.
 start_time = time.time()
 img_loader = ImageLoader(img_info)
 img_loader.load_all_images()
-elapsed = time.time() - start_time
+elapsed = round(time.time() - start_time, 2)
 print ('Data successfully loaded in {} seconds.'.format(elapsed))
+
+# Get the deep CNN model for the given data.
+model = build_model(img_info.num_channels, img_info.img_width,
+                    img_info.img_height)
+
+# Compile the model with SGD + momentum.
+print ('Compiling module...')
+start_time = time.time()
+sgd = SGD(lr=learning_rate, decay=decay, momentum=momentum, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd)
+elapsed = round(time.time() - start_time, 2)
+print ('Done in {} seconds.'.format(elapsed))
+
+# Train the model.
+start_time = time.time()
+if not data_augmentation:
+  print ('Training without data augmentation.')
+  model.fit(train_data, train_labels,
+            validation_data=(test_data, test_labels), batch_size=batch_size,
+            nb_epoch=num_epochs, shuffle=True,
+            show_accuracy=True, verbose=1)
+else:
+  print ('Training with additional data augmentation.')
+  # TODO: implement this!
+elapsed = round(time.time() - start_time, 2)
+print ('Finished training in {} seconds.'.format(elapsed))
