b5fe4fca2d620d747cce80c9e90d2faf4b655dde
Richard Teammco <teammco@cs.utexas.edu>
2016-03-02 10:30:33 -0600

    Updated trainin parameters. Also updated gitignore.

--- run.py
+++ run.py
@@ -39,7 +39,7 @@ def elapsed_time(start_time):
 
 # Hyperparameters:
 batch_size = 32
-num_epochs = 10 #50 #200
+num_epochs = 50 #200
 data_augmentation = False
 learning_rate = 0.01
 decay = 1e-6
@@ -47,10 +47,11 @@ momentum = 0.9
 
 # Set the data parameters and image source paths.
 # TODO: use more clearly-defined image path files.
-img_info = ImageInfo(25, 100, 20)
-img_info.set_image_dimensions(64, 64, 1)  # img width, height, channels
+img_info = ImageInfo(25, 1160, 20)
+img_info.set_image_dimensions(128, 128, 1)  # img width, height, channels
 img_info.load_image_classnames('test/classnames.txt')
-img_info.load_train_image_paths('test/trainImNames.txt')
+#img_info.load_train_image_paths('test/trainImNames.txt')
+img_info.load_train_image_paths('test/extraTrainImNames.txt')
 img_info.load_test_image_paths('test/test1ImNames.txt')
 
 # Load the images into memory and preprocess appropriately.
